6222e7a0fb722879e531f455368de573b1e71c73
juliangranda <juliangranda@hotmail.com>
2020-04-24 14:14:19 -0500

    desestructuracion con props

--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -6,11 +6,14 @@ class Ninjas extends Component{
 
         //uso de props(propiedades del componente)
         //console.log(this.props);
+
+        //desestructuracion
+        const { name, age, belt} = this.props;
         return(
             <div className = "ninja">
-                <div>Name: { this.props.name }</div>
-                <div>Age: { this.props.age }</div>
-                <div>Belt: { this.props.belt }</div>
+                <div>Name: { name }</div>
+                <div>Age: { age }</div>
+                <div>Belt: { belt }</div>
             </div>
         )
     }
