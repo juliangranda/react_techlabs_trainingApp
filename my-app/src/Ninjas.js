af95d3c118104a5430d0d5193ec762fb9550c7ca
juliangranda <juliangranda@hotmail.com>
2020-04-24 14:59:24 -0500

    UI and container component

--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -1,40 +1,27 @@
-import React, { Component} from 'react';
+import React from 'react';
 
-//new component
-class Ninjas extends Component{
-    render(){
+const Ninjas = (props) => {
 
-        //uso de props(propiedades del componente)
-        //console.log(this.props);
-
-        //desestructuracion
-        //const { name, age, belt} = this.props;
-
-        //outputting list
-        const { ninjas } = this.props
-        const ninjaList = ninjas.map( ninja => {
-            return(
-                <div className = "ninja" key = { ninja.id} >
-                    <div>Name: { ninja.name }</div>
-                    <div>Age: { ninja.age }</div>
-                    <div>Belt: { ninja.belt }</div>
-                </div>
-            )
-        })
-        
-
-
+    const { ninjas } = props
+    const ninjaList = ninjas.map( ninja => {
         return(
-            <div className = "ninja-list">
-                { ninjaList }
+            <div className = "ninja" key = { ninja.id} >
+                <div>Name: { ninja.name }</div>
+                <div>Age: { ninja.age }</div>
+                <div>Belt: { ninja.belt }</div>
             </div>
         )
+    })
+    
 
-    }
+
+    return(
+        <div className = "ninja-list">
+            { ninjaList }
+        </div>
+    )
 }
 
-//export the component important
-export default Ninjas;
 
-//when we import the component in the main component
-//we use < Ninjas />
+
+export default Ninjas;
