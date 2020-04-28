cb4595a118e59e70be1a322df67267a9e1148bff
juliangranda <juliangranda@hotmail.com>
2020-04-27 20:24:47 -0500

    create a route parameter

--- my-app2/poketimes/src/App.js
+++ my-app2/poketimes/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Post from './components/post';
 class App extends Component {
   render(){
     return(
@@ -13,6 +14,7 @@ class App extends Component {
           <Route exact path = '/' component = {Home}/>
           <Route path = '/about' component = {About}/>
           <Route path = '/contact' component = {Contact}/>
+          <Route path = "/:post_id" component={Post}></Route>
         </div>
       </BrowserRouter>
     );
